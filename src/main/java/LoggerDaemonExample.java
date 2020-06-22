9fb8c96d7834d579b83fd2aaf7b2495e82c264ca
Santhosh Babu M R <santhosh.infotec@gmail.com>
2020-06-22 07:36:31 +0530

    updated

--- src/main/java/LoggerDaemonExample.java
+++ src/main/java/LoggerDaemonExample.java
@@ -1,7 +1,20 @@
 import java.io.*;
 
-public class LoggerDaemonExample {
+// Write end dead exception using PipedInputStream java
+// - https://stackoverflow.com/questions/5453525/write-end-dead-exception-using-pipedinputstream-java
 
+public class LoggerDaemonExample {
+    public static void main(String[] args) throws IOException {
+        PipedWriter pipedWriter = new LoggerDaemon().getPipedWriter();
+        pipedWriter.append("Remember:");
+        pipedWriter.append("You are what you think!!");
+        pipedWriter.close();
+
+        PrintWriter printWriter = new LoggerDaemon().getPrintWriter();
+        printWriter.println("Remember:");
+        printWriter.println("You are what you think!!");
+        printWriter.close();
+    }
 }
 
 class LoggerDaemon extends Thread {
@@ -20,12 +33,23 @@ class LoggerDaemon extends Thread {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                pin.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    public Writer getWriter() {
-        PipedWriter pout = new PipedWriter();
-        BufferedWriter bw = new BufferedWriter(pout);
+    public PrintWriter getPrintWriter() throws IOException{
+        PipedWriter pout = new PipedWriter(pin);
+        PrintWriter pw = new PrintWriter(pout);
+        return pw;
+    }
+
+    public PipedWriter getPipedWriter() throws IOException{
+        PipedWriter pout = new PipedWriter(pin);
         return pout;
     }
 
